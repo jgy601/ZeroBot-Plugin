e65cb818712fc7c201a21e2b80c9bfeeb272ba4b
medicago087 <medicago087@gmail.com>
2023-03-29 20:03:50 +0800

    antirecall合并转发改为一条消息

--- plugin/antirecall/antirecall.go
+++ plugin/antirecall/antirecall.go
@@ -43,6 +43,38 @@ func init() {
 			msg = append(msg, message.CustomNode(username, uid, img))
 			ctx.SendPrivateForwardMessage(su, msg)
 		})
+	// 注释掉了合并转发ver
+	// engine.OnNotice().SetBlock(false).
+	// 	Handle(func(ctx *zero.Ctx) {
+	// 		if ctx.Event.NoticeType != "group_recall" && ctx.Event.NoticeType != "friend_recall" {
+	// 			return
+	// 		}
+	// 		su := zero.BotConfig.SuperUsers[0]
+	// 		now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
+	// 		raw := ctx.GetMessage(message.NewMessageIDFromInteger(ctx.Event.MessageID.(int64))).Elements
+	// 		rawmsg := raw.String()
+	// 		uid := ctx.Event.UserID
+	// 		botid := ctx.Event.SelfID
+	// 		if uid == su || uid == botid || strings.Contains(rawmsg, ",type=flash") || strings.Contains(rawmsg, "CQ:reply") && strings.Contains(rawmsg, "撤回") && uid == su {
+	// 			return
+	// 		}
+	// 		gid := ctx.Event.GroupID
+	// 		botname := zero.BotConfig.NickName[0]
+	// 		username := ctx.CardOrNickName(uid)
+	// 		msg := make(message.Message, 10)
+	// 		msg = append(msg, message.CustomNode(botname, botid, "捕捉到了一条撤回的消息！\n"+"时间:"+now))
+	// 		if gid != 0 {
+	// 			groupname := ctx.GetGroupInfo(gid, true).Name
+	// 			msg = append(msg, message.CustomNode(botname, botid, "来自群聊:["+groupname+"]("+strconv.FormatInt(gid, 10)+")\n来自用户:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是源消息："))
+	// 		} else {
+	// 			msg = append(msg, message.CustomNode(botname, botid, "来自私聊:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是源消息："))
+	// 		}
+	// 		if strings.Contains(rawmsg, "CQ:record") {
+	// 			defer ctx.SendChain(raw...)
+	// 		}
+	// 		msg = append(msg, message.CustomNode(username, uid, raw))
+	// 		ctx.SendPrivateForwardMessage(su, msg)
+	// 	})
 	engine.OnNotice().SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
 			if ctx.Event.NoticeType != "group_recall" && ctx.Event.NoticeType != "friend_recall" {
@@ -58,20 +90,24 @@ func init() {
 				return
 			}
 			gid := ctx.Event.GroupID
-			botname := zero.BotConfig.NickName[0]
 			username := ctx.CardOrNickName(uid)
-			msg := make(message.Message, 10)
-			msg = append(msg, message.CustomNode(botname, botid, "捕捉到了一条撤回的消息！\n"+"时间:"+now))
 			if gid != 0 {
 				groupname := ctx.GetGroupInfo(gid, true).Name
-				msg = append(msg, message.CustomNode(botname, botid, "来自群聊:["+groupname+"]("+strconv.FormatInt(gid, 10)+")\n来自用户:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是源消息："))
+				if strings.Contains(rawmsg, "CQ:record") {
+					defer ctx.SendChain(raw...)
+				}
+				text := message.UnescapeCQCodeText("捕捉到了一条撤回的消息！\n" +
+					"时间:" + now + "\n" +
+					"来自群聊:[" + groupname + "](" + strconv.FormatInt(gid, 10) + ")\n" +
+					"来自用户:[" + username + "](" + strconv.FormatInt(uid, 10) + ")\n" +
+					"以下是源消息：\n" + rawmsg)
+				ctx.SendPrivateMessage(su, message.ParseMessageFromString(text))
 			} else {
-				msg = append(msg, message.CustomNode(botname, botid, "来自私聊:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是源消息："))
+				text := message.UnescapeCQCodeText("捕捉到了一条撤回的消息！\n" +
+					"时间:" + now + "\n" +
+					"来自私聊:[" + username + "](" + strconv.FormatInt(uid, 10) + ")\n" +
+					"以下是源消息：\n" + rawmsg)
+				ctx.SendPrivateMessage(su, message.ParseMessageFromString(text))
 			}
-			if strings.Contains(rawmsg, "CQ:record") {
-				defer ctx.SendChain(raw...)
-			}
-			msg = append(msg, message.CustomNode(username, uid, raw))
-			ctx.SendPrivateForwardMessage(su, msg)
 		})
 }
